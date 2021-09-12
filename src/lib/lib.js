b9ce68d5ff32a8f0f967be575da63f97e42c6aa6
erol <erool89@hotmail.com>
2021-09-12 21:49:41 +0200

    Fix email & password validator

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,11 +1,7 @@
-export const emailValidator = (email) => {
-  return email === "" || !email.includes("@");
+export const isEmailValid = (email) => {
+  return email !== "" || email.includes("@");
 };
 
-export const passwordValidator = (password) => {
-  return password === "" || password.length < 6;
-};
-
-export const passwordConfirmationValid = (password, confirm) => {
-  return password !== confirm;
+export const isPasswordValid = (password) => {
+  return password !== "" || password.length >= 6;
 };
